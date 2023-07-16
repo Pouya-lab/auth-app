ea907c6c3e010c1a81d17c4805b6d4c957c1b538
pouya <pouyabh102@gmail.com>
2023-07-16 17:53:22 +0330

    useragent were added to users ctrls

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler')
 const User = require('../model/user')
 const bcrypt = require('bcryptjs')
 const { generateToken } = require('../util')
+var parser = require('ua-parser-js')
 
 exports.registerUser = asyncHandler( 
     async (req , res ) =>{
@@ -25,11 +26,16 @@ exports.registerUser = asyncHandler(
             throw new Error('Email has already been registered')
         }
 
+        //get user agent
+        const ua = parser(req.headers['user-agent'])
+        const userAgent = [ua.ua]
+
         //create new user
         const user = await User.create({
             name , 
             email ,
-            password
+            password,
+            userAgent
         })
 
         //generate token
